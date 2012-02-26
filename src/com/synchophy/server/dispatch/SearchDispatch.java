e07832bc2808fadbd2b3c48f8600bdd438902bf8
Tim Thomas <tim@buyplaytix.com>
2012-02-26 15:47:40 -0600

    modified the search dispatch

--- src/com/synchophy/server/dispatch/SearchDispatch.java
+++ src/com/synchophy/server/dispatch/SearchDispatch.java
@@ -1,6 +1,8 @@
 package com.synchophy.server.dispatch;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,37 +20,56 @@ public class SearchDispatch extends AbstractDispatch {
 		// tag
 		// playlist
 
-		String type = getRequiredParameter(request, "t");
 		String query = getRequiredParameter(request, "q");
 
-		if (type.equals("artist")) {
-			return DatabaseManager.getInstance()
-					.query("select distinct(artist_sort) " + "from song "
-							+ "where artist_sort like ? "
-							+ "order by upper(artist_sort)",
-							new String[] { "%" + query + "%" },
-							new String[] { "name" });
-		} else if (type.equals("album")) {
-			return DatabaseManager.getInstance().query(
-					"select album_sort, artist_sort " + "from song "
-							+ "where album_sort like ? "
-							+ " group by album_sort, artist_sort "
-							+ " order by upper(album_sort)",
-					new String[] { "%" + query + "%" },
-					new String[] { "name", "artist" });
+		Map results = new HashMap();
+		results.put(
+				"artist",
+				DatabaseManager.getInstance().query(
+						"select distinct(artist_sort) " + "from song "
+								+ "where artist_sort like ? "
+								+ "order by upper(artist_sort)",
+						new String[] { "%" + query + "%" },
+						new String[] { "name" }));
+		results.put(
+				"album",
+				DatabaseManager.getInstance().query(
+						"select album_sort, artist_sort " + "from song "
+								+ "where album_sort like ? "
+								+ " group by album_sort, artist_sort "
+								+ " order by upper(album_sort)",
+						new String[] { "%" + query + "%" },
+						new String[] { "name", "artist" }));
 
-		} else if (type.equals("track")) {
-			return DatabaseManager.getInstance().query(
-					"select trim(LEADING '0' FROM title_sort), album_sort, artist_sort "
-							+ "from song " + "where title_sort like ? "
-							+ " group by title_sort, album_sort, artist_sort "
-							+ " order by upper(title_sort)",
-					new String[] { "%" + query + "%" },
-					new String[] { "name", "album", "artist" });
+		results.put(
+				"track",
+				DatabaseManager
+						.getInstance()
+						.query("select trim(LEADING '0' FROM title_sort), album_sort, artist_sort "
+								+ "from song "
+								+ "where title_sort like ? "
+								+ " group by title_sort, album_sort, artist_sort "
+								+ " order by upper(title_sort)",
+								new String[] { "%" + query + "%" },
+								new String[] { "name", "album", "artist" }));
 
-		}
+		results.put(
+				"tag",
+				DatabaseManager.getInstance().query(
+						"select tag_name " + "from tag "
+								+ "where tag_name like ? ",
+						new String[] { "%" + query + "%" },
+						new String[] { "name" }));
 
-		return null;
+		results.put(
+				"station",
+				DatabaseManager.getInstance()
+						.query("select name " + "from station "
+								+ "where name like ? ",
+								new String[] { "%" + query + "%" },
+								new String[] { "name" }));
+		return results;
+
 	}
 
 }
